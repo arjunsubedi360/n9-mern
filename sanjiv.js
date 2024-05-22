cee580906bc27fc10d69b23b3a6e7bc047a095f8
sanjiv <sanjiv0114@gmail.com>
2024-05-22 08:15:35 +0545

    new change

--- sanjiv.js
+++ sanjiv.js
@@ -1,5 +1,9 @@
 let fullName = "sanjiv sharma";
-let age = "8";
+let age = 8;
 let education = "BBS-management";
 let email = "sanjiv0114@gmail.com";
 let address = "gaurighat gausala";
+
+console.log(fullName)
+console.log(age)
+console.log(education)
