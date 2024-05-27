b83c35cfc9a1f686c4899a660c1e5f284ebe0aa5
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-05-27 07:41:44 +0545

    refactor

--- playground/test.js
+++ playground/test.js
@@ -1,2 +1,8 @@
-const remainder = 10 % 3;
-console.log("remainder", remainder)
+const check  = 6 <= 5; // will not check type
+console.log("check", check)
+
+if(check) { //left hand always store variable name in js
+    console.log(" this is truthy")
+} else {
+    console.log("this is falsy")
+}
