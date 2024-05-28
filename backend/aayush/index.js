22823d7096c54d4755ba7c60246ff1e1f86d8d24
Aayush Shrestha <shrest197@uni.coventry.ac.uk>
2024-05-28 20:04:55 +0545

    .

---
+++ backend/aayush/index.js
@@ -0,0 +1,20 @@
+import express, { request, response } from "express";
+import {post} from "./post.js";
+
+const app = express();
+const port = 4000; 
+
+app.use(express.json());
+
+app.get("/posts",(request, response) => {
+    response.status(200).json({post});
+})
+
+app.post("/posts",(request, response) => {
+    post.push(request.body);
+    response.status(201).json(post);
+})
+
+app.listen(port, ()=> {
+    console.log(`Listening at port ${port}`);
+});
