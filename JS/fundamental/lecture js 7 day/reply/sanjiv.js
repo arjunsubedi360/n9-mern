7039062d4f390a8acc8be583aa8d3f92efbd1dea
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-05-30 08:11:33 +0545

    lecture 8

--- JS/fundamental/lecture js 7 day/reply/sanjiv.js
+++ JS/fundamental/lecture js 7 day/reply/sanjiv.js
@@ -1,10 +1,10 @@
-let num = parseInt(prompt("Enter an integer: "));
-let range = parseInt(prompt("Enter a range: "));
-
+let num = parseInt(prompt("Enter an integer: "), 2);
+let range = parseInt(prompt("Enter a range: "), 2);
+console.log("range", range)
 //creating a multiplication table
 for (let i = 1; i <= range; i++) {
   let res = i * num;
   table = document.createElement(`table${i}`);
-  table.innerHTML = `${num} * ${range} = ${res}`;
+  table.innerHTML = `${num} * ${i} = ${res}`;
   document.querySelector("div").append(table);
 }
