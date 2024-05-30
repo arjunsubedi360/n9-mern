909c59c5a6473817c7de843d3c1c3dfdbea689c6
sxnmgxr <sujanmagar05825@gmail.com>
2024-05-30 19:07:17 +0545

    update

---
+++ sujan-backend/users.js
@@ -0,0 +1,27 @@
+export const users = [
+    {
+        id: 1,
+        name: "Arjun Subedi",
+        address: "Kathmandu"
+    },
+    {
+        id: 2,
+        name: "Nabin Lama",
+        address: "Jhapa"
+    },
+    {
+        id: 3,
+        name: "Sujan Magar",
+        address: "Khotang"
+    },
+    {
+        id: 4,
+        name: "Ayush Shrestha",
+        address: "Bhaktapur"
+    },
+    {
+        id: 5,
+        name: "Nigma Tendi Sherpa",
+        address: "Mustang"
+    }
+]
