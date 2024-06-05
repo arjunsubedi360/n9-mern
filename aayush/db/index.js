d3a38709f0c0cc110eab79a69d73a44d1a5712e6
Aayush Shrestha <shrest197@uni.coventry.ac.uk>
2024-06-05 22:54:48 +0545

    database configure

--- aayush/db/index.js
+++ aayush/db/index.js
@@ -1,5 +1,5 @@
 import  mongoose from "mongoose";
-const username = encodeURIComponent("rajendrakarki0614")
-const password = encodeURIComponent("2005Rajendrakarki0614@");
+const username = encodeURIComponent("Aayush")
+const password = encodeURIComponent("H6UGBb35NzBKmCd3");
 const dbName = "mero-order";
 mongoose.connect(`mongodb+srv://${username}:${password}@rajendrakarki.8qtyuyy.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=RajendraKarki`)
