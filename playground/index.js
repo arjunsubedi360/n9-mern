641d26d08679bd27f67aadc5a0ee11a730c2e8ee
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-06-07 07:48:39 +0545

    lecture rest

---
+++ playground/index.js
@@ -0,0 +1,54 @@
+console.log(" I am running");
+/* 
+spread
+rest
+destructure
+ternary operator
+*/
+
+// const obj1 = { id: 1, name: "Arjun", age: 16};
+// const obj2 = {job: "developer"}
+
+// const obj3 = {
+//     ...obj1,
+//    ...obj2
+// };
+
+// console.log(obj3)
+
+// spread
+// function sum(a,b, ...rest) { // function define param is variable of function
+//     console.log(a,b)
+//     console.log(...rest)
+// };
+
+// sum(1, 2,3,54,5,6)
+
+// destructure
+// const person = {
+//   id: 1,
+//   name: "Arjun",
+//   age: 16,
+//   children: { name: "Ayush" },
+// };
+
+// const {
+//   id,
+//   name: personName,
+//   children: childs,
+// } = person;
+
+// console.log(personName);
+
+// ternary operator
+
+// const isDone = false ? "YO  true part print": "Yo false part ho";
+// console.log(isDone)
+// const user = ""
+// const canLogin = user ? "You can login": "Unauthorize";
+// console.log(canLogin)
+
+
+
+const array = [1,3,5]
+
