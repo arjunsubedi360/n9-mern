b096d71369c823d355dd59ee23855afc582ba0da
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-06-10 19:27:19 +0545

    array destrucutre

--- playground/index.js
+++ playground/index.js
@@ -50,5 +50,20 @@ ternary operator
 
 
 
-const array = [1,3,5]
+// const array = [1,3,5,6,7,8,9];
+// const array1 = [10,11];
+// const [a,b,c, ...rest] = array;
 
+// const array2 = [1,3,5,[1,5,[8,87]]];
+// console.log(array2.flat());
+
+//destructure in array
+const array = [1,3,5,6,7,8,9];
+const [a,,c] = array;
+console.log(a,c)
+
+//spread in array
+const array1  =[1,3,[3,5]];
+const array2 = [5,6];
+const array3 = [...array1, ...array2];
+console.log(array3)
