77bea565e4226e7985bbcfe4f925097760818937
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-06-10 19:53:45 +0545

    add package-lock

--- backend/models/Table.js
+++ backend/models/Table.js
@@ -2,7 +2,14 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 const tableSchema = new Schema(
   {
-    slug: String,
+    slug: {
+      type: String,
+      default: "slug",
+      unique: true,
+      required: true,
+      minLength: [2, "Cannot be less than 2 character"],
+      maxLength: [20, "Cannot tbe more than 50 character"]
+    },
   },
   {
     timestamps: true,
