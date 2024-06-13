bc96a1bfa88ca610996d03d0657750095f3dd942
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-06-13 18:47:53 +0545

    try catch

--- sanjiv/script.js
+++ sanjiv/script.js
@@ -1,23 +1,54 @@
 const GIT_HUB_API = "https://api.github.com/users/mesanjiv";
 
 async function fetchGitHubUser() {
-  try {
-    const response = await fetch(GIT_HUB_API);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    const userImage = document.createElement("img");
-    const userName = document.createElement("h2");
-    userImage.src = data.avatar_url;
-    userImage.alt = "User Image";
+ try {
+   const response =  await fetch(GIT_HUB_API);
+   console.log(response);
+   if (!response.ok) {
+     throw new Error("Network response was not ok");
+   }
+   const data = await response.json();
 
-    userName.textContent = data.name || "Name not provided";
+   if(!data.location) {
+    throw new Error("He has no location! He should be hacker")
+   };
+   
+   const userImage = document.createElement("img");
+   const userName = document.createElement("h2");
+   userImage.src = data.avatar_url;
+   userImage.alt = "User Image";
 
-    document.body.appendChild(userName);
-    document.body.appendChild(userImage);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+   userName.textContent = data.name || "Name not provided";
+
+   document.body.appendChild(userName);
+   document.body.appendChild(userImage);
+ } catch (error) {
+  console.log(error.message);
+ }
 }
 fetchGitHubUser();
+
+// const user = {
+//   id: 1,
+//   name: "Sanjiv Sharma",
+//   email: "sanjiv@gmail.com",
+//   password: "Password@1",
+// };
+
+// const dbUser = ["sanjiv@gmail.com"];
+// function login(user) {
+//   if (!user) {
+//     throw new Error("User does not exist");
+//   }
+
+//   const emailExists = dbUser.includes(user.email);
+
+//   if(!emailExists) {
+//     throw new Error("Wrong Email")
+//   }
+
+//   return "Successfully login"
+// }
+
+
+// login(user);
