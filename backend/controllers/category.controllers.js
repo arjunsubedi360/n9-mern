3abaaf6118035214c38b1ffde3cecba704ee9f1c
Aayush Shrestha <shrest197@uni.coventry.ac.uk>
2024-06-13 20:20:12 +0545

    services modified

--- backend/controllers/category.controllers.js
+++ backend/controllers/category.controllers.js
@@ -1,4 +1,5 @@
-import { getSingleCategory } from "../services/index.js";
+import { createSingleCategory, getSingleCategory } from "../services/index.js";
+import { responseData } from "../utils/successResponseData.js";
 
 export const getCategory =async (request, response) => {
     const id = request.params.id;
@@ -6,3 +7,26 @@ export const getCategory =async (request, response) => {
 
     response.send(data);
 };
+
+const createCategory = async (request, response) => {
+   try {
+     const input = request.body;
+ 
+     const slug = slugify(input.name);
+ 
+     const CategoryExists = await createSingleCategory({ slug });
+     console.log(CategoryExists);
+     if (CategoryExists) {
+       throw new Error("Table already exists");
+       const data = await createSingleCategory({...input, slug})
+
+       responseData({
+        data,
+        message: lang.CREATE("Category"),
+        statusCode: HttpStat
+       })
+     }
+   } catch (error) {
+    
+   }
+}
--- backend/services/category.services.js
+++ backend/services/category.services.js
@@ -1,5 +1,16 @@
 import Category from "../models/Category.js";
 
+export const createSingleCategory = async (payload) => {
+  try {
+    console.log({ payload });
+
+    const data = await Category.create(payload);
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const getSingleCategory = async (id) => {
   const data = await Category.findOne({ _id: id });
   console.log(data);
