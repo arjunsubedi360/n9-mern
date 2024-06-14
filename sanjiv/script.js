d55b90a1e940afd6fa23b9c03fbeda847aa39315
sanjiv <sanjiv0114@gmail.com>
2024-06-14 06:43:32 +0545

    sanjiv folder change

--- sanjiv/script.js
+++ sanjiv/script.js
@@ -8,13 +8,17 @@ async function fetchGitHubUser() {
     }
     const data = await response.json();
     const userImage = document.createElement("img");
+    const userEmail = document.createElement("email");
     const userName = document.createElement("h2");
+
     userImage.src = data.avatar_url;
     userImage.alt = "User Image";
 
     userName.textContent = data.name || "Name not provided";
+    userEmail.textContent = data.email || "email not found";
 
     document.body.appendChild(userName);
+    document.body.appendChild(userEmail);
     document.body.appendChild(userImage);
   } catch (error) {
     console.error("Error fetching data:", error);
