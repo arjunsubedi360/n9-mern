727e3c58d1de721913a7d9860e784ba461893c66
Nirtesh Gautam <nirteshgautam@gmail.com>
2024-06-16 16:20:11 +0545

    Added About.html

--- node-setup/index.js
+++ node-setup/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.listen(4000, function(){
+app.listen(4000, function () {
     console.log("I am running at port 4000")
 })
+// console.log('HAVE FUN');
+// console.log(`${express}`);
+console.log(`${app}`)
