9670d422faf606469f26136fa60c6c64ab887d63
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-06-17 07:48:44 +0545

    import and export

--- node-setup/index.js
+++ node-setup/index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+import express from "express";
+import { users } from "./models/users.js";
 
 const app = express();
 const port = 4000;
 
-app.listen(port, function(){
-    console.log(`I am running at port ${port}`)
-})
+app.get("/users", (request, response) => {
+  console.log(users);
+  response.json(users);
+});
+
+app.listen(port, function () {
+  console.log(`I am running at port ${port}`);
+});
