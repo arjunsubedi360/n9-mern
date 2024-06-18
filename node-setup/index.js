15b1b1307549a3a7b618259b67e2851d0f6ea587
Nirtesh Gautam <nirteshgautam@gmail.com>
2024-06-18 22:36:49 +0545

    Some changes in GET method express

--- node-setup/index.js
+++ node-setup/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { users } from "./models/users.js";
+import { posts } from "./models/posts.js";
 
 const app = express();
 const port = 4000;
@@ -7,6 +8,10 @@ const port = 4000;
 app.get("/users", (request, response) => {
   console.log(users);
   response.json(users);
+});
+app.get("/posts", (req, res) => {
+  console.log(posts);
+  res.json(posts);
 });
 
 app.listen(4000, function () {
