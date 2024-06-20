528164e120fac374b49fdf265d97bc787c802394
Rajendra Karki <rajendrakarki0614@gmail.com>
2024-06-20 18:51:06 +0545

    validations

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import AdminRouter from "./routes/v1/admins/index.js";
 import { notFound } from "./middlewares/notFound.js";
 import { connectDb } from "./db/index.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 const port = 3001;
@@ -9,12 +10,14 @@ const port = 3001;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+
 app.use("/auth/admin/v1", AdminRouter);
 
+app.use(errorHandler);
 app.use(notFound);
 
 function startServer() {
-  connectDb()
+  // connectDb()
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
