218dbae3afaf1cb6e0775c5619712629833c0791
sxnmgxr <sujanmagar05825@gmail.com>
2024-06-24 18:46:38 +0545

    completing the process

--- backend/routes/v1/admins/index.js
+++ backend/routes/v1/admins/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import UserRouter from "./user.routes.js";
 import CategoryRouter from "./category.routes.js";
 import TableRouter from './table.routes.js';
+import MenuRouter from "./menu.route.js";
 
 const router = Router();
 
 router.use("/users", UserRouter);
 router.use("/categories", CategoryRouter);
 router.use("/tables", TableRouter);
+router.use("/menu", MenuRouter);
 
 export default router;
