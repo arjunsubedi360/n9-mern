66c47f49600b472d8d1e3f894b517fce343eacf0
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-06-28 19:54:58 +0545

    error handler

---
+++ backend/middlewares/validate.js
@@ -0,0 +1,10 @@
+export const validate = (schema) => {
+  return (req, res, next) => {
+    const { value, error } = schema.validate(req.body);
+    if (error) {
+      next(error);
+    }
+    req.body = value;
+    next();
+  };
+};
