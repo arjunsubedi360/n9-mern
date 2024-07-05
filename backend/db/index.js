d15fb4629f8c7256b9864dd6788b5c62c336d419
sanjiv <sanjiv0114@gmail.com>
2024-07-05 08:26:03 +0545

    front end of shoe store

--- backend/db/index.js
+++ backend/db/index.js
@@ -11,3 +11,4 @@ const password = encodeURIComponent(dbPassword);
     console.log("Error connecting db")
   }
 }
+
---
+++ node_practise/routes/v1/admins/users.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { createSingleUser, getUsersList } from "../../../controllers/index.js"; // Importing from the index.js of controllers
+import { validate } from "../../../middlewares/validate.js";
+import { createUser } from "../../../validations/user.validations.js";
+import { authentication } from "../../../middlewares/authentication.js";
+import { authorization } from "../../../middlewares/authorization.js";
+
+const router = Router();
+
+router.post(
+  "/",
+  validate(createUser),
+  authentication,
+  authorization,
+  createSingleUser
+);
+
+router.get(
+  "/",
+  getUsersList
+);
+
+export default router;
