aa7649d4f279060a69687931908b504d1f75a813
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-07-10 19:54:07 +0545

    progress bar on fallback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,52 @@
+// import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+// import Navbar from "./layouts/Navbar";
+// import AppRoutes from "./routes/AppRoutes";
+// import './styles.css';
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <AppRoutes />
+//     </BrowserRouter>
+//   );
+// }
+
+
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-import ContactForm from "./Form";
+// import Home from "./Home";
+// import About from "./About";
+// import Contact from "./Contact";
+// import ContactForm from "./Form";
+import './styles.css'
+import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link> {/* This is just mentioning to go about page */}
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/contact-form">Contact Form</Link>
-          </li>
+      <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          Logo
+        </div>
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
-      </nav>
+      </div>
+    </nav>
 
-      <Routes>
+      {/* <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/contact-form" element={<ContactForm />} />
 
-      </Routes>
+      </Routes> */}
+      <AppRoutes/>
     </BrowserRouter>
   );
 }
