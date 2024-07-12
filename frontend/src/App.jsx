ee9bc21f9593a7a7935d44345246b6bad6aed6d4
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-07-12 20:36:20 +0545

    react router and lazy route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,15 @@
-// import React from "react";
-// import { BrowserRouter } from "react-router-dom";
-// import Navbar from "./layouts/Navbar";
-// import AppRoutes from "./routes/AppRoutes";
-// import './styles.css';
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <AppRoutes />
-//     </BrowserRouter>
-//   );
-// }
-
-
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-// import Home from "./Home";
-// import About from "./About";
-// import Contact from "./Contact";
-// import ContactForm from "./Form";
-import './styles.css'
+import { BrowserRouter as Router } from "react-router-dom";
+import "./styles.css";
 import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./layouts/Navbar";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <nav className="navbar">
-      <div className="navbar-container">
-        <div className="logo">
-          Logo
-        </div>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </div>
-    </nav>
-
-      {/* <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/contact-form" element={<ContactForm />} />
-
-      </Routes> */}
-      <AppRoutes/>
-    </BrowserRouter>
+    <>
+      <Router>
+        <Navbar />
+        <AppRoutes />
+      </Router>
+    </>
   );
 }
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
-import React, { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import LoadingBar from "../components/LoadingBar";
 
-const Home = React.lazy(() => import("../components/Home"));
-const About = React.lazy(() => import("../components/About"));
-const Contact = React.lazy(() => import("../components/Contact"));
+const Home = lazy(() => import("../components/Home"));
+const About = lazy(() => import("../components/About"));
+const Contact = lazy(() => import("../components/Contact"));
 
 const AppRoutes = () => {
+ 
   return (
     <Suspense fallback={<LoadingBar/>}>
       <Routes>
