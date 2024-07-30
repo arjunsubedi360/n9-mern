97184a022627b3874d48aae8649421cbda4439e4
sanjiv <sanjiv0114@gmail.com>
2024-07-30 22:06:03 +0545

    vite frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        primary: 'var(--primary-color)',
-      },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
