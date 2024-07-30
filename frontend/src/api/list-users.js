74580ea6e06817c6f1d39543a21f5831a031da1f
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-07-30 07:51:36 +0545

    tailwind setup

---
+++ frontend/src/api/list-users.js
@@ -0,0 +1,12 @@
+export function listUsers() {
+  return [
+    {
+      id: 1,
+      name: "Arjun",
+    },
+    {
+      id: 2,
+      name: "Sanjiv",
+    },
+  ];
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
-}
-
+};
