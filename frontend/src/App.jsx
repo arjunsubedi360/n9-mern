522a0bf3a9fce0e02fe7d2b2c419b3a523c12e87
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-08-01 07:09:09 +0545

    useState, useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from "react";
-// import DashboardPage from './components/DashboardPage';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   const [name, setName] = useState(null);
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log("I am running");
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  }, [count, name]);
 
-  const handleChange = () => setName("Arjun");
   return (
-    // <DashboardPage/>
-    <>
-      <div className="text-center">
-        <p>{name}</p>
-        <button onClick={handleChange}>Click Me</button>
-      </div>
-    </>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setName("Arjun")}>Name changed</button>
+    </div>
   );
 }
 
