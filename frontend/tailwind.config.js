77cf553a742ef9f4b2ed1732ad84723c6d7fd64d
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-08-02 20:42:41 +0545

    dashboard

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,30 +8,6 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
-    colors: {
-      primary: "var(--primary-color)",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      spacing: {
-        128: "32rem",
-        144: "36rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
   },
   plugins: [],
 };
