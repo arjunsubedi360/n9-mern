3e30338b248c2f6ee473d3ee89a79c9d45b044d6
NabinLama21 <nabinplama01@gmail.com>
2024-08-06 15:05:09 +0545

    icon changes

---
+++ frontend/src/components/icons/TickIcon.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const TickIcon = () => {
+  return (
+    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={3}
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        className="h-4 w-4 text-blue-500"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4.5 12.75l6 6 9-13.5"
+                        />
+                        </svg>
+  )
+}
+
+export default TickIcon
