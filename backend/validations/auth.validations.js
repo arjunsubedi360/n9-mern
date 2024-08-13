86307ce653ef6e844c15282e919e3b5a0ad9f266
Arjun Subedi <arjun.subedi@generaltechnologies.com.np>
2024-08-13 07:05:37 +0545

    feat: register

--- backend/validations/auth.validations.js
+++ backend/validations/auth.validations.js
@@ -1,7 +1,15 @@
 import Joi from "joi";
 
-export const login = Joi.object({
+export const loginValidation = Joi.object({
   email: Joi.string().min(2).max(30).required().label("Email"),
   password: Joi.string().min(2).max(50).required().label("Password"),
 });
 
+export const registerValidation = Joi.object({
+  name: Joi.string().min(2).max(30).required().label("Name"),
+  email: Joi.string().min(2).max(50).required().label("Email"),
+  age: Joi.number().min(2).max(50).required().label("Age"),
+  isMarried: Joi.boolean(),
+  role: Joi.string().min(2).max(50).required().label("Role"),
+  password: Joi.string().min(2).max(50).required().label("Password"),
+});
